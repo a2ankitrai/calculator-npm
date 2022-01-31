4a79ebfbbe5d5b36f932165be26d4dfe5681e09a
Ankit Rai <a2.ankitrai@gmail.com>
2022-01-31 21:53:09 +0100

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export const Greeter = (name: string) => `Hello ${name}`;
+
+export class Calculator {
+  public add(x: number, y: number) {
+    return x + y;
+  }
+
+  public subtract(x: number, y: number) {
+    return x - y;
+  }
+
+  public multiply(x: number, y: number) {
+    return x * y;
+  }
+
+  public divide(x: number, y: number) {
+    return x * y;
+  }
+}
